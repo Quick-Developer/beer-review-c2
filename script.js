5b78a57ca3d8b0398c0a6238d62b76929342d768
unknown <klirs123@gmail.com>
2018-04-10 20:39:05 +0300

    add moodle

--- script.js
+++ script.js
@@ -1,54 +1,74 @@
-function addBeer (_name, _category, _rate){
-	beers.push( {name: _name, category : _category,  rate : _rate });
-}
-function renderBeers (){
-	$( 'ul.beers-list' ).empty();
-	for (i = 0; i < beers.length; ++i) {
-		$('ul.beers-list').append("<li>" + beers[i].name + ' \ ' + beers[i].category +  ' \ ' + beers[i].rate + "</li>");
+var renderBeers = function () {
+	$('ul.beers-list').empty();
+	for (var i = 0; i < beers.length; ++i) {
+		$('ul.beers-list').append("<li>" + beers[i].name + ' \ ' + beers[i].category + ' \ ' + beers[i].rate + "</li>");
 	}
 }
 
-function sortBeers (_sorted, _sortedAsc){
-	if (_sorted && _sortedAsc) {
-		beers.sort(SortDesByRate);		
-		sortedAsc = false;
+
+
+var BeerReviewApp = function () {
+	var beers = [];
+	var sorted = false;
+	var sortedAsc = false;
+
+	var addBeer = function (_name, _category, _rate) {
+		beers.push({ name: _name, category: _category, rate: _rate });
 	}
-	else {
-		beers.sort(SortAscByRate);
-		sortedAsc = true;
+	var updateBeers = function () {
+		$('ul.beers-list').empty();
+		for (var i = 0; i < beers.length; ++i) {
+			$('ul.beers-list').append("<li>" + beers[i].name + ' \ ' + beers[i].category + ' \ ' + beers[i].rate + "</li>");
+		}
+
 	}
-	
+	var sortBeers = function () {
+		sorted = true;
+		if (sorted && sortedAsc) {
+			beers.sort(SortDesByRate);
+			sortedAsc = false;
+		}
+		else {
+			beers.sort(SortAscByRate);
+			sortedAsc = true;
+		}
+	}
+
+	var SortAscByRate = function (a, b) {
+		var aRate = a.rate.toLowerCase();
+		var bRate = b.rate.toLowerCase();
+		return ((aRate < bRate) ? -1 : ((aRate > bRate) ? 1 : 0));
+	}
+
+	var SortDesByRate = function (a, b) {
+		var aRate = a.rate.toLowerCase();
+		var bRate = b.rate.toLowerCase();
+		return ((aRate > bRate) ? -1 : ((aRate < bRate) ? 1 : 0));
+	}
+	return {
+		addBeer: addBeer,
+		updateBeers: updateBeers,
+		sortBeers : sortBeers
+	}
 }
 
-function SortAscByRate(a, b){
-  var aRate = a.rate.toLowerCase();
-  var bRate = b.rate.toLowerCase(); 
-  return ((aRate < bRate) ? -1 : ((aRate > bRate) ? 1 : 0));
-}
-function SortDesByRate(a, b){
-  var aRate = a.rate.toLowerCase();
-  var bRate = b.rate.toLowerCase(); 
-  return ((aRate > bRate) ? -1 : ((aRate < bRate) ? 1 : 0));
-}
-
-$( '.post-beer' ).click(function () {
-	addBeer ( $( '.beer-input').val(), $( '.category-input' ).val(), $( "#sel1" ).val());
-	renderBeers();
+$('.post-beer').click(function () {
+	app.addBeer($('.beer-input').val(), $('.category-input').val(), $("#sel1").val());
+	app.updateBeers();
 	$('.category-input').val('');
 	$('.beer-input').val('');
-	$( '#sel1' ).prop('selectedIndex', 0); 
+	$('#sel1').prop('selectedIndex', 0);
+});
+
+$('.sort-beer').click(function () {
+	app.sortBeers();
 	
+	app.updateBeers();
 });
 
-$( '.sort-beer' ).click(function () {	
-	sortBeers (sorted, sortedAsc);
-	sorted = true;
-	renderBeers();
-});
+var app = BeerReviewApp();
 
-var sorted = false;
-var sortedAsc = false;
-var beers = [];
+
 
 //beers.sort(function(a, b) {
 //    return parseFloat(a.rate) - parseFloat(b.rate);
